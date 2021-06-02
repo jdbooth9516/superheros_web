4cc8ca7c28cd29db38029d8a5ee465dcd93e67e3
Jeremiah Booth <jeremiah.boothphotog@gmail.com>
2021-06-02 12:00:38 -0700

    views and urls 50% completed

--- superheros/heros/models.py
+++ superheros/heros/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Hero(models.Model):
+    name = models.CharField(max_length= 50)
+    alter = models.CharField(max_length=50)
+    primary_power = models.CharField(max_length=100)
+    secondary_power = models.CharField(max_length=100)
+    catchphrase = models.TextField()
+
+
+    def __str__(self):
+        return self.name
